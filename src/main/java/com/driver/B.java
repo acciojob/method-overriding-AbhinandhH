aa0a0116f60c399fe1e49fd5fc21594713b28700
Abhinandh Haridas <abhinandhabhi53@gmail.com>
2023-01-12 11:34:00 +0530

    called the method meth of class A using object of class B

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,5 +1,9 @@
 package com.driver;
 
-public class B extends A{
+class B extends A{
+//    @Override
+//    String meth() {
+//        return "Method is overridden in Extendend class B";
+//    }
 
 }
